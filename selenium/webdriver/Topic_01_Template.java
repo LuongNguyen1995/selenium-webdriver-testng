66b4253a4217796eb79e6f3b925acbb24bc3e744
LuongNguyen1995 <luong.epu.dtvt1@gmail.com>
2022-01-19 21:43:47 +0700

    Update template

---
+++ selenium/webdriver/Topic_01_Template.java
@@ -0,0 +1,53 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Template {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+
+	@Test
+	public void TC_02() {
+		
+	}
+
+	@Test
+	public void TC_03() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInSecon (long second) {
+		try {
+			Thread.sleep( second * 1000);
+		}
+		catch(InterruptedExcepton e){
+			e.printStackTrace();
+		}
+		
+	}
+}
