0e3b5a46bcd8e3a3e95978dde3fb1ae458364934
LuongNguyen1995 <luong.epu.dtvt1@gmail.com>
2022-02-21 23:47:22 +0700

    Update topic 22,23 Wait : Static Wait and Implicit Wait

---
+++ selenium/webdriver/Topic_23_Wait_Part_4_StaticWait.java
@@ -0,0 +1,94 @@
+package webdriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.SendKeys;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_23_Wait_Part_4_StaticWait {
+	WebDriver driver;
+	WebDriverWait explicitWait;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.startsWith("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else if (osName.startsWith("Mac")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver_mac");
+		} else  {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver_linux");
+		}
+		
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void TC_01_Timeout_Less_Than_Find_Element_Display() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.xpath("//button")).click();
+		sleepInSecond(3);
+		//After click - take 5s to Helloworld display 
+		//Wait until hello world display
+		Assert.assertTrue(driver.findElement(By.xpath("//h4[text()='Hello World!']")).isDisplayed());	
+		System.out.println(driver.findElement(By.xpath("//h4[text()='Hello World!']")).getText());
+		
+		//isDisplay ko liên quan đến implicitWait. Có hiển thị trên UI : return true, k hiển thị thì return false
+	}
+	
+	@Test
+	public void TC_02_Timeout_Less_Equal_Element_Display() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.xpath("//button")).click();
+		sleepInSecond(5);
+		//After click - take 5s to Helloworld display 
+		//Wait until hello world display
+		Assert.assertTrue(driver.findElement(By.xpath("//h4[text()='Hello World!']")).isDisplayed());	
+		System.out.println(driver.findElement(By.xpath("//h4[text()='Hello World!']")).getText());
+
+	}
+	
+	@Test
+	public void TC_03_Timeout_Greate_Than_Element_Display() {
+		driver.get("https://automationfc.github.io/dynamic-loading/");
+		driver.findElement(By.xpath("//button")).click();
+		sleepInSecond(8);
+		//After click - take 5s to Helloworld display 
+		//Wait until hello world display
+		Assert.assertTrue(driver.findElement(By.xpath("//h4[text()='Hello World!']")).isDisplayed());	
+		System.out.println(driver.findElement(By.xpath("//h4[text()='Hello World!']")).getText());
+
+	}
+
+	@Test
+	public void TC_04() {
+		
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInSecond (long second) {
+		try {
+			Thread.sleep( second * 1000);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		
+	}
+}
